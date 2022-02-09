b35c2c26cb02163910403818c7d83bea5ae9947f
Astrral <53837488+airmonxd@users.noreply.github.com>
2022-02-08 20:48:57 -0500

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,22 @@
+import React from "https://dev.jspm.io/react";
+import ReactDOMServer from "https://dev.jspm.io/react-dom/server";
+import { Application } from "https://deno.land/x/abc@v1.3.3/mod.ts";
+import Home from "./pages/home.jsx";
+const app = new Application();
+
+app.use((next) =>
+  (c) => {
+    let e = next(c);
+    if (React.isValidElement(e)) {
+      e = ReactDOMServer.renderToString(e);
+    }
+    return e;
+  }
+);
+
+app.get("/", () => {
+  return <Home />;
+})
+  .start({ port: 8080 });
+
+console.log(`server listening on http://localhost:8080`);
